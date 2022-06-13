0c5f9c81a0849a3fe314a4eeaac9bb15cac36628
danil <gcpesochks@gmail.com>
2022-06-13 03:13:00 +0300

    Pages dynamic

--- index.js
+++ index.js
@@ -8,24 +8,45 @@ const app = express()
 
 const db = require("./db");
 const bodyParser = require("body-parser");
+const ItemsService = require("./services/items");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.set('view engine', 'ejs');
 // db.sequelize.sync({ force: true })
 
 app.use('/static', express.static('./static'))
-
-app.get('/', (req, res) => {
-    res.render('pages/index');
+// странички
+app.get('/', async (req, res) => {
+    const items = await ItemsService.getForMain()
+    res.render('pages/index', {
+        items
+    });
+})
+app.get('/catalog', async (req, res) => {
+    const options = req.query.category
+    const search = req.query.search
+    console.log(options)
+    let items = {}
+    if (options) {
+        items = await ItemsService.getByCategory(options)
+    } else if (search) {
+        items = await ItemsService.getByName(search)
+    } else {
+        items =  await ItemsService.getAll(options)
+    }
+    res.render('pages/catalog', {
+        items
+    });
 })
 
-app.get('/catalog/:id', (req, res) => {
-    res.render('pages/catalog');
+app.get('/catalog/:id', async (req, res) => {
+    const item = await ItemsService.getById(req.params.id)
+    res.render('pages/card', {
+        item
+    });
 })
 
-app.get('/catalog', (req, res) => {
-    res.render('pages/catalog');
-})
+
 
 app.get('/contacts', (req, res) => {
     res.render('pages/contacts');
@@ -39,7 +60,7 @@ app.get('/cart', (req, res) => {
     res.render('pages/cart');
 })
 
-
+// Добавление
 app.post('/additem', async (req, res) => {
     try {
         const item = req.body
