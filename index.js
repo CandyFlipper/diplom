2584d083f9ffe133c1a2a40a4fd3501df199d5c0
CandyFlipper <mrjmr1337@mail.ru>
2022-06-14 00:03:29 +0800

    cart_updated

--- index.js
+++ index.js
@@ -6,21 +6,71 @@ const PORT = process.env.PORT || 8080
 
 const app = express()
 
-const db = require('./db')
-const Items = db.shop
+const db = require("./db");
+const bodyParser = require("body-parser");
+const ItemsService = require("./services/items");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.set('view engine', 'ejs');
+//db.sequelize.sync({ force: true })
 
-app.get('/', (req, res) => {
-    res.send('HELLO ZALUPA + GOVNOOOOO')
+app.use('/static', express.static('./static'))
+// странички
+app.get('/', async (req, res) => {
+    const items = await ItemsService.getForMain()
+    res.render('pages/index', {
+        items
+    });
 })
-// if (req.url === '/') {
-//     fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, data) => {
-//         if (err) {
-//         throw err
-//         }
-    
-//         res.writeHead(200, {
-//             'Content-Type': 'text/html'
-//         })
-//         res.end(data)
-//     })
-app.listen(PORT, () => console.log('server started on post ${PORT}'))
+app.get('/catalog', async (req, res) => {
+    const options = req.query.category
+    const search = req.query.search
+    console.log(options)
+    let items = {}
+    if (options) {
+        items = await ItemsService.getByCategory(options)
+    } else if (search) {
+        items = await ItemsService.getByName(search)
+    } else {
+        items =  await ItemsService.getAll(options)
+    }
+    res.render('pages/catalog', {
+        items
+    });
+})
+
+app.get('/catalog/:id', async (req, res) => {
+    const item = await ItemsService.getById(req.params.id)
+    res.render('pages/card', {
+        item
+    });
+})
+
+
+
+app.get('/contacts', (req, res) => {
+    res.render('pages/contacts');
+})
+
+app.get('/about', (req, res) => {
+    res.render('pages/about');
+})
+
+app.get('/cart', (req, res) => {
+    res.render('pages/cart');
+})
+
+// Добавление
+app.post('/additem', async (req, res) => {
+    try {
+        const item = req.body
+        const itemCreated = await db.Items.create(item)
+        return res.send(itemCreated);
+    } catch (e) {
+        res.status(500).send({
+            message: e.message || "eror"
+        });
+    }
+
+})
+app.listen(PORT, () => console.log(`server started on post ${PORT}`))
---
+++ services/items.js
@@ -0,0 +1,36 @@
+const db = require("../db");
+const {Op} = require("sequelize");
+
+module.exports = class ItemsService {
+    static getAll (options) {
+        return db.Items.findAll(options)
+    }
+    static getById (id = 1) {
+        return db.Items.findOne({
+            where: {
+                id
+            }
+        })
+    }
+    static getByCategory (category) {
+        return db.Items.findAll({
+            where: {
+                category
+            }
+        })
+    }
+    static getByName (name) {
+        return db.Items.findAll({
+            where: {
+                itemname: {
+                    [Op.iLike]: `%${name}%`
+                }
+            }
+        })
+    }
+    static getForMain () {
+        return db.Items.findAll({
+            limit: 4
+        })
+    }
+}
